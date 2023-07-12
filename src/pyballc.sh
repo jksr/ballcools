77c1ed11909bb14b4ecb55aa0dca17e24b38bb43
jksr <jksr.tw@gmail.com>
2023-07-12 13:11:17 -0700

    separate pyballc to its own package

--- src/pyballc.sh
+++
@@ -1,4 +0,0 @@
-swig -c++ -python pyballc.i
-
-libs="allc.cc ballc.cc ballc_index.cc ballc_index_core.cc ballc_iterator.cc context_matcher.cc meta_indexing.cc timer.cc utils.cc"
-g++  pyballc_wrap.cxx $libs -fPIC -shared  $(python3-config --includes) -I${CONDA_PREFIX}/include -w -std=c++11 -O3  -L${CONDA_PREFIX}/lib -lhts -ldeflate -lbz2 -lpthread -lz -lstdc++ -o _pyballc.so
