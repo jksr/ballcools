32b5ea4a132abe964ba2c8532c12724a1872e6f6
jksr <jksr.tw@gmail.com>
2023-07-12 15:06:45 -0700

    add test script

---
+++ test/prep_tar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Ensure two arguments are provided
+if [ $# -ne 3 ]; then
+    echo "Error: This script requires 3 arguments."
+    echo "Usage: $0 <source_directory> <target_directory> <chrom_size_path>"
+    exit 1
+fi
+
+# Variables for source and target directories
+src=$1
+tgt=$2
+chrsz=$3
+
+# Copy .allc.tsv.gz files from source to target directory
+cp ${src}/*.allc.tsv.gz ${tgt}
+cp ${src}/*.allc.tsv.gz.tbi ${tgt}
+cp ${chrsz} ${tgt}
+
+# Loop over each .allc.tsv.gz file in source directory and run command
+for file in ${src}/*.allc.tsv.gz
+do
+    base=$(basename $file .allc.tsv.gz)
+    ballcools a2b ${file} ${tgt}/${base}.ballc ${chrsz}
+    ballcools index ${tgt}/${base}.ballc
+done
+
+# Create a tarball of the target directory
+tar -czvf ${tgt}.tar.gz ${tgt}
+
+echo "${tgt}.tar.gz is the file generated. you may delete folder ${tgt}"
